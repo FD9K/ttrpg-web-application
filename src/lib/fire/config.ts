f9151e16fba8984735e10ffcb4d4e70eef8ecc78
Jack Ryan <jack.dixon.ryan@gmail.com>
2021-07-26 14:25:07 -0600

    support for email/password authentication.

---
+++ src/lib/fire/config.ts
@@ -0,0 +1,25 @@
+
+// firebase goes in here. 
+import firebase from "firebase";
+
+const fb = (function module() {
+  "use strict";
+
+  (async function configure() {
+    const firebaseConfig = {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  })();
+})();
+
+export const firestore = firebase.firestore();
+export const realtime = firebase.database();
+export const auth = firebase.auth();
