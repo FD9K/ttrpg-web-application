1bebfd88187494ea86ffcc531e23968d34af1513
Jack Ryan <jack.dixon.ryan@gmail.com>
2021-08-05 15:03:02 -0600

    A little teeny tiny foray into worn equipment. But I think I should deviate to focus on ux again and get things looking better.

--- src/routes/users/[userId]/characters/[characterId].svelte
+++ src/routes/users/[userId]/characters/[characterId].svelte
@@ -2,6 +2,7 @@
   import Inventory from "../../../../components/tyler-ttrpg/inventory.svelte";
   import Runepouch from "../../../../components/tyler-ttrpg/runepouch.svelte";
   import RuneAdder from "../../../../components/tyler-ttrpg/rune-addition.svelte";
+  // import WornEquipment from "../../../../components/tyler-ttrpg/"
 
   let selectedRune; 
 
