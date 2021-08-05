3f68b4d358730b1d3acf36533ace1503d6796187
Jack Ryan <jack.dixon.ryan@gmail.com>
2021-08-05 14:14:32 -0600

    Rune addition has selection now.

---
+++ src/components/tyler-ttrpg/rune-addition.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+  const runes = [
+    { runeName: "air",
+      type: "elemental",
+    },
+    { runeName: "water",
+      type: "elemental",
+    },
+    { runeName: "earth",
+      type: "elemental",
+    },
+    { runeName: "fire",
+      type: "elemental",
+    },
+    { runeName: "smoke",
+      type: "combination",
+    },
+    { runeName: "mist",
+      type: "combination",
+    },
+    { runeName: "dust",
+      type: "combination",
+    },
+    { runeName: "mud",
+      type: "combination",
+    },
+    { runeName: "steam",
+      type: "combination",
+    },
+    { runeName: "slag",
+      type: "combination",
+    },
+    { runeName: "shock",
+      type: "essential",
+    },
+    { runeName: "lava",
+      type: "essential",
+    },
+    { runeName: "ice",
+      type: "essential",
+    },
+    { runeName: "solar",
+      type: "essential",
+    },
+    { runeName: "impulse",
+      type: "minorCatalyst",
+    },
+    { runeName: "body",
+      type: "minorCatalyst",
+    },
+    { runeName: "nature",
+      type: "minorCatalyst",
+    },
+    { runeName: "force",
+      type: "majorCatalyst",
+    },
+    { runeName: "mind",
+      type: "majorCatalyst",
+    },
+    { runeName: "law",
+      type: "majorCatalyst",
+    },
+    { runeName: "power",
+      type: "legendaryCatalyst",
+    },
+    { runeName: "soul",
+      type: "legendaryCatalyst",
+    },
+    { runeName: "astral",
+      type: "legendaryCatalyst",
+    }
+  ]
+
+</script>
+
+<div class="shadow-md m-2 rounded-lg border border-gray-600 overflow-hidden">
+  <div class="bg-purple-500 p-2">
+    <p class="text-lg font-bold text-gray-100 pl-2">Add Runes</p>
+  </div>
+  <div class="flex justify-center items-center p-2 m-1 uppercase font-bold text-xm">
+    <p>Rune Selected: ⭐︁</p>
+    <div class="flex justify-center items-center flex-wrap">
+      <!-- card? -->
+    </div>
+  </div>
+</div>
--- src/routes/users/[userId]/characters/[characterId].svelte
+++ src/routes/users/[userId]/characters/[characterId].svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import Inventory from "../../../../components/tyler-ttrpg/inventory.svelte";
   import Runepouch from "../../../../components/tyler-ttrpg/runepouch.svelte";
+  import RuneAdder from "../../../../components/tyler-ttrpg/rune-addition.svelte";
 </script>
 
 <Runepouch />
+<RuneAdder />
